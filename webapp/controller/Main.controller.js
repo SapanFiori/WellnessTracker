d0327dce7cda0cb58c3a420d81d86f64304f36fa
Sapan 1 <sapan@nagarro.com>
2023-04-13 13:04:06 +0000

    New commit sapan

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -13,8 +13,86 @@ sap.ui.define([
                 var oRootPath = jQuery.sap.getModulePath("ns01.moody"); // your resource root 
                 var oImageModel = new sap.ui.model.json.JSONModel({ "path": oRootPath, });
                 this.getOwnerComponent().setModel(oImageModel, "imageModel");
+                this.getView().byId("id_sleep").focus();
+                var smileyPath = this.getOwnerComponent().getModel("imageModel").getData().path;
+                var HappyPath = smileyPath + "/picture/health.png";
+                this.getView().byId("smiley").setWidth("120%");
+                this.getView().byId("smiley").setSrc(HappyPath);
             },
             onPress: function () {
+                var oScreen = this.getView();
+                oScreen.setBusy(true);
+                // this.getView().byId("pie").setVisible(false);
+                // this.getView().byId("chartContainer").setVisible(true);
+                // this.getView().byId("pie").setWidth("0%");
+                // this.getView().byId("chartContainer").setWidth("100%");
+                var oModel = this.getView().getModel();
+                var payload = {};
+                var currentDate = new Date();
+                let x = Date.now();
+                let tmpTime = x.toString();
+                payload.UserID = tmpTime;
+                payload.Crdat = currentDate;
+                payload.Sleep = this.getView().byId("id_sleep").getValue();
+                payload.Meeting = this.getView().byId("id_meet").getValue();
+                payload.Travel = this.getView().byId("id_travel").getValue();
+                payload.Hobbies = this.getView().byId("id_hobby").getValue();
+                payload.PhyActvt = this.getView().byId("id_exrcs").getValue();
+                payload.WorkHour = this.getView().byId("id_work").getValue();
+                payload.Others = this.getView().byId("id_other").getValue();
+                payload.IsActiveEntity = true;
+                oModel.create("/UsrData", payload, {
+                    success: function (oData, response) {
+                        var toastSuccess = "Response Generated Successfully";
+                        this.getView().byId("panel1").setExpanded(true);
+                        this.getView().byId("area1").focus();
+                        MessageToast.show(toastSuccess);
+                        var aiResponse = oData.Suggest;
+                        this.getView().byId("area1").setText(aiResponse);
+                        //pie chart code
+                        var ovalues = {
+                            values:
+                                [{ category: "Sleep Time", value: oData.Sleep },
+                                { category: "Screen Time", value: oData.Meeting },
+                                { category: "Commute Time", value: oData.Travel },
+                                { category: "WorkOut Time", value: oData.PhyActvt },
+                                { category: "Leisure Time", value: oData.Hobbies },
+                                { category: "Job Time", value: oData.WorkHour }]
+                        };
+                        var smileyPath = this.getView().getModel("imageModel").getData().path;
+                        this.getView().byId("smiley").setWidth("60%");
+                        if (oData.MentalScore >= 8) {
+                            var HappyPath = smileyPath + "/picture/happy.png";
+                            this.getView().byId("smiley").setSrc(HappyPath);
+                        }
+                        else if (oData.MentalScore >= 5 && oData.MentalScore <= 7) {
+                            var NeutralPath = smileyPath + "/picture/neutral.png";
+                            this.getView().byId("smiley").setSrc(NeutralPath);
+                        }
+                        else if (oData.MentalScore >= 1 && oData.MentalScore <= 4) {
+                            var SadPath = smileyPath + "/picture/sad.png";
+                            this.getView().byId("smiley").setSrc(SadPath);
+                        }
+                        var scoreTxt = "Your Wellness Score";
+                        this.getView().byId("scoreText").setText(scoreTxt);
+
+                        var scoreNum = oData.MentalScore + "/10";
+                        this.getView().byId("scoreNumber").setText(scoreNum);
+
+                        var oJsonModel = new sap.ui.model.json.JSONModel();
+                        oJsonModel.setData(ovalues);
+                        this.pieChartCreate(oJsonModel); //function call to create pie chart
+                        this.getView().byId("idPieChart").removeAllFeeds();
+                        oScreen.setBusy(false);
+                    }.bind(this),
+                    error: function (oError) {
+                        oScreen.setBusy(false);
+                        var toastFail = "Failed to Generate response from AI";
+                        MessageToast.show(toastFail);
+                    }
+                });
+            },
+            onPressWeekly: function () {
                 var oScreen = this.getView();
                 oScreen.setBusy(true);
                 var oModel = this.getView().getModel();
@@ -22,85 +100,49 @@ sap.ui.define([
                 var currentDate = new Date();
                 let x = Date.now();
                 let tmpTime = x.toString();
-                payload.UserID = tmpTime.slice(8, 13);
+                this.onPressReset();
+                payload.UserID = tmpTime;
                 payload.Crdat = currentDate;
-                payload.Sleep = this.getView().byId("id_sleep").getValue();
-                payload.Meeting = this.getView().byId("id_meet").getValue();
-                payload.Travel = this.getView().byId("id_travel").getValue();
-                payload.Hobbies = this.getView().byId("id_hobby").getValue();
-                payload.PhyActvt = this.getView().byId("id_exrcs").getValue();
-                payload.WorkHour = this.getView().byId("id_work").getValue();
-                payload.IsActiveEntity = true;
-                oModel.create("/UsrData", payload, {
-                    success: function (oData, response) {
-                        oScreen.setBusy(false);
-                        var toastSuccess = "Response Generated Successfully";
-                        MessageToast.show(toastSuccess);
-                        var aiResponse = oData.Suggest;
-                        this.getView().byId("area1").setValue(aiResponse);
-                        //pie chart code
-                         var ovalues = {
-                           values: 
-                          [{ category: "Sleep Time",  value: oData.Sleep },
-                           { category: "Screen Time", value: oData.Meeting },
-                           { category: "Commute Time",value: oData.Travel },
-                           { category: "WorkOut Time",value: oData.PhyActvt },
-                           { category: "Leisure Time", value: oData.Hobbies},
-                           { category: "Job Time", value: oData.WorkHour}]
-                        };
-              
-                        var oJsonModel = new sap.ui.model.json.JSONModel();
-                        oJsonModel.setData(ovalues);
-                        this.pieChartCreate( oJsonModel ); //function call to create pie chart
-                        this.getView().byId("idPieChart").removeAllFeeds();
-                    }.bind(this),
-                    error: function (oError) {
-                      oScreen.setBusy(false);
-                      var toastFail = "Failed to Generate response from AI";
-                      MessageToast.show(toastFail);
-                    }
-                });
-            },
-            onPressWeekly: function () {
-                var oModel = this.getView().getModel();
-                var payload = {};
-                var currentDate = new Date();
-                let x = Date.now();
-                let tmpTime = x.toString();
-                payload.UserID = tmpTime.slice(8, 13);
-                payload.Crdat = currentDate;
-                payload.Sleep = this.getView().byId("id_sleep").getValue();
-                payload.Meeting = this.getView().byId("id_meet").getValue();
-                payload.Travel = this.getView().byId("id_travel").getValue();
-                payload.Hobbies = this.getView().byId("id_hobby").getValue();
-                payload.PhyActvt = this.getView().byId("id_exrcs").getValue();
-                payload.WorkHour = this.getView().byId("id_work").getValue();
                 payload.Suggest = "Week"
                 payload.IsActiveEntity = true;
                 oModel.create("/UsrData", payload, {
                     success: function (oData, response) {
+                        oScreen.setBusy(false);
+                        this.getView().byId("panel1").setExpanded(true);
+                        this.getView().byId("area1").focus();
                         var aiResponse = oData.Suggest;
-                        this.getView().byId("area1").setValue(aiResponse);
+                        this.getView().byId("area1").setText(aiResponse);
                         //pie chart code
                         var ovalues = {
-                            values: 
-                           [{ category: "Sleep Time",  value: oData.Sleep },
-                            { category: "Screen Time", value: oData.Meeting },
-                            { category: "WorkOut Time",value: oData.WorkHour },
-                            { category: "Commute Time",value: oData.Travel },
-                            { category: "WorkOut Time",value: oData.PhyActvt },
-                            { category: "Leisure Time", value: oData.Hobbies},
-                            { category: "Job Time", value: oData.WorkHour}]
-                         };
-               
-                         var oJsonModel = new sap.ui.model.json.JSONModel();
-                         oJsonModel.setData(ovalues);
-                         this.pieChartCreate( oJsonModel ); //function call to create pie chart
+                            values:
+                                [{ category: "Sleep Time", value: oData.Sleep },
+                                { category: "Screen Time", value: oData.Meeting },
+                                { category: "WorkOut Time", value: oData.WorkHour },
+                                { category: "Commute Time", value: oData.Travel },
+                                { category: "WorkOut Time", value: oData.PhyActvt },
+                                { category: "Leisure Time", value: oData.Hobbies },
+                                { category: "Job Time", value: oData.WorkHour }]
+                        };
+
+                        var oJsonModel = new sap.ui.model.json.JSONModel();
+                        oJsonModel.setData(ovalues);
+                        this.pieChartCreate(oJsonModel); //function call to create pie chart
+                        this.getView().byId("idPieChart").removeAllFeeds();
                     }.bind(this),
                     error: function (oError) {
+                        oScreen.setBusy(false);
 
                     }
                 });
+            },
+            onPressReset: function () {
+                this.getView().byId("id_sleep").setValue("");
+                this.getView().byId("id_meet").setValue("");
+                this.getView().byId("id_travel").setValue("");
+                this.getView().byId("id_hobby").setValue("");
+                this.getView().byId("id_exrcs").setValue("");
+                this.getView().byId("id_work").setValue("");
+                this.getView().byId("id_other").setValue("");
             },
             pressFb: function () {
                 window.location.href = "https://www.facebook.com";
@@ -120,9 +162,9 @@ sap.ui.define([
             pressInsta: function () {
                 window.location.href = "https://www.Instagram.com";
             },
-            pieChartCreate: function( oJsonModel ) {
-            this.getView().byId("idPieChart").removeAllFeeds();
-            this.getView().setModel(oJsonModel, "oJModel" );
+            pieChartCreate: function (oJsonModel) {
+                this.getView().byId("idPieChart").removeAllFeeds();
+                this.getView().setModel(oJsonModel, "oJModel");
             }
         });
     });
